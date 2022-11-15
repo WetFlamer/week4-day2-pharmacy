e647a85a1ae2f027daebfb33e6fbf69458604035
WetFlamer <xamidchabaev04@gmail.com>
2022-11-15 17:08:58 +0300

    Поиск по ID и категории для Лекарств

--- controllers/Medicines.controller.js
+++ controllers/Medicines.controller.js
@@ -7,10 +7,22 @@ module.exports = medicinesController = {
             category: req.body.category,
             price: req.body.price,
             soldbyPres: req.body.soldbyPres
+        }).then( () => {
+            res.json('Лекарство добавлено')
         })
     },
     getMeds: (req, res) => {
-        Medicines.find().then( meds => {
+        Medicines.find().populate('category').then( meds => {
+            res.json(meds)
+        })
+    },
+    getMedsbyId: (req, res) => {
+        Medicines.findById(req.params.medId).populate('category').then(med => {
+            res.json(med)
+        })
+    },
+    getMedsbyCategory: (req, res) => {
+        Medicines.find({category: req.params.categoryId}).populate('category').then(meds => {
             res.json(meds)
         })
     },
--- routes/Medicines.route.js
+++ routes/Medicines.route.js
@@ -3,10 +3,12 @@ const medicinesController = require("../controllers/Medicines.controller");
 
 const router = Router();
 // ADMIN
-router.post('/admin/create/medicines', medicinesController.addMed)
+router.post('/admin/medicines/create', medicinesController.addMed)
 router.patch('/admin/edit/:medId', medicinesController.editMeds)
 router.delete('/admin/delete/:medId', medicinesController.deleteMeds)
 // CLIENTS
 router.get('/medicines', medicinesController.getMeds)
+router.get('/medicines/:medId', medicinesController.getMedsbyId)
+router.get('/medicines/category/:categoryId', medicinesController.getMedsbyCategory)
 
 module.exports = router
